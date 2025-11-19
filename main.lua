19ebb55152740b342efe32c827b9a1da253fa63d
Ava :3 <138967687+avamalino@users.noreply.github.com>
2025-11-19 06:13:09 +0000

    i'm just messing things up atp

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ cpml = require "cpml"
 g3d = require "g3d"
 
 
---more demo code to make sure the server and libraries work (code by groverbuger https://github.com/groverburger/g3d?tab=readme-ov-file)
+--more demo code to make sure the server and libraries work (code by groverbuger )
 local earth = g3d.newModel("assets/sphere.obj", "assets/earth.png", {4,0,0})
 local moon = g3d.newModel("assets/sphere.obj", "assets/moon.png", {4,5,0}, nil, 0.5)
 local background = g3d.newModel("assets/sphere.obj", "assets/starfield.png", nil, nil, 500)
