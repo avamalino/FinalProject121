58650125f59b83e6d23cdd1274cdcc92cbaed80f
Sonny Trucco <htrucco@ucsc.edu>
2025-12-11 05:58:25 +0000

    added back another crucial file

---
+++ src/lang/en.lua
@@ -0,0 +1,22 @@
+return {
+    inventory = {
+        name = "Inventory",
+        items = "%d Items",
+        footer_text = "Hit ESC to close",
+    },
+    rooms = {
+        room1 = {
+            controls = "Controls:\nWASD/Arrows - Move\nSpace - Interact\nZ - Undo\nI - Inventory",
+            inventory = "Inventory",
+            pickup = "Pick Up",
+            undo = "Undo",
+        },
+        room2 = {
+            controls = "Controls:\nWASD/Arrows - Move\nSpace - Interact\nZ - Undo\nI - Inventory",
+        },
+        room3 = {
+            controls = "Controls:\nWASD/Arrows - Move\nSpace - Interact\nZ - Undo\nI - Inventory",
+        },
+        ending = {},
+    }
+}
